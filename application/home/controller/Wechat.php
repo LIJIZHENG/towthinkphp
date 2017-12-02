bc7531690ab0fcb25d08a11b9f6a63f6a2e3fb89
root <3436803285qq.com>
2017-12-03 00:50:49 +0800

    dsd

--- application/home/controller/Wechat.php
+++ application/home/controller/Wechat.php
@@ -16,7 +16,7 @@ class Wechat extends Controller
     public function info(){
         Session::get('return_rul',url('home/wechat/info'));
         if (!Session::has('openid')){
-            $appID = "wx3409b27359e3922d";
+            $appID = "wx0160f586424189a1";
             $callback_url = url('home/wechat/callback','',true,true);
             $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appID}&redirect_uri={$callback_url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
             $this->redirect($url);
@@ -26,8 +26,8 @@ class Wechat extends Controller
     }
     //回调页
     public function callback(){
-        $appID = "wx3409b27359e3922d";
-        $secret = "5245caab2aa4fedbae60ec558b19d818";
+        $appID = "wx0160f586424189a1";
+        $secret = "254a1fe1c2ec600171ae5ab33739dbb3";
         $code = request()->get('code');
         $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$appID}&secret={$secret}&code={$code}&grant_type=authorization_code";
         $str = file_get_contents($url);
